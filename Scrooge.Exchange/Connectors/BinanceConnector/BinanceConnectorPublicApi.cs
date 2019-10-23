6cb4730387a281a8f37528c15a81438da21fb4d2
petelinmn <petelinmn@gmail.com>
2019-10-23 18:27:13 +0500

    Exchange.Connectors has merged to Exchange

---
+++ Scrooge.Exchange/Connectors/BinanceConnector/BinanceConnectorPublicApi.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Scrooge.Exchange.Connectors.BinanceConnector
+{
+    public partial class BinanceConnector
+    {
+        public async Task<bool> Ping()
+        {
+            var response = await SendRequest<JObject>("ping", ApiVersion.Version1, ApiMethodType.None, HttpMethod.Get);
+            return response.Type == JTokenType.Object;
+        }
+
+        public async Task<IEnumerable<PriceInfo>> TickerAllPrices()
+        {
+            return await SendRequest<List<PriceInfo>>("ticker/allPrices", ApiVersion.Version1, ApiMethodType.None, HttpMethod.Get);
+        }
+
+
+        public class PriceInfo
+        {
+            public string Symbol { get; set; }
+            public decimal Price { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Symbol}: {Price}";
+            }
+        }
+    }
+}
