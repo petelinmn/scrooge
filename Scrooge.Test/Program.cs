7b943758ad7dbcd2cc545bb6411e487110c0e75f
petelinmn <petelinmn@gmail.com>
2019-10-23 08:16:07 +0500

    initial

---
+++ Scrooge.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scrooge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
