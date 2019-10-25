ac2de3f6f6a243c82c4babbf95c1b7913a3959ea
petelinmn <petelinmn@gmail.com>
2019-10-25 17:55:40 +0500

    first setup fluentmigrator

---
+++ Scrooge.Migrations/Migrations/Migration201910231927.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using FluentMigrator.SqlServer;
+
+namespace Scrooge.Migrations.Migrations
+{
+    [Migration(201910242000)]
+    public class Migration201910242000 : Migration
+    {
+        public override void Up()
+        {
+            Create.Schema("scrooge");
+        }
+
+        public override void Down()
+        {
+            Delete.Schema("scrooge");
+        }
+    }
+
+}
---
+++ Scrooge.Migrations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using FluentMigrator;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+using Scrooge.Migrations.Migrations;
+
+namespace Scrooge.Migrations
+{
+    public class Program
+    {
+        private static IServiceProvider _serviceProvider;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+
+
+            _serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            var connectionString = "User ID=postgres;Password=1984;Host=localhost;Port=5432;Database=scrooge;";
+
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb =>
+                {
+                    rb.AddPostgres()
+                        .WithGlobalConnectionString(connectionString)
+                        .ScanIn(typeof(Migration201910242000).Assembly).For.Migrations();
+                })
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+
+        }
+        
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+
+    }
+}
