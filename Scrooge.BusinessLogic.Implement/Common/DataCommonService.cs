4c4b3967d20b666e744299be860b69dd037f256d
Petelin Andrey <apeteln6821@gmail.com>
2020-03-01 14:09:39 +0500

    добавил функцию общих валют

--- Scrooge.BusinessLogic.Implement/Common/DataCommonService.cs
+++ Scrooge.BusinessLogic.Implement/Common/DataCommonService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Scrooge.Exchange.Connectors;
 using Scrooge.DataAccess.Common;
@@ -24,6 +25,8 @@ namespace Scrooge.BusinessLogic.Implement.Common
 
             var assetMarketMap = GetAssetMarketMap(allAssets, tickerResult);
 
+            var unionAssetMarketMap = UnionGetAssetMarketMap(allAssets, tickerResult);
+
             return false;
         }
 
@@ -45,6 +48,42 @@ namespace Scrooge.BusinessLogic.Implement.Common
             }
 
             return assetMarketMap;
+        }
+        private static Dictionary<Asset, List<string>> UnionGetAssetMarketMap(IEnumerable<Asset> assets, IList<PriceInfo> tickerResult)
+        {
+            var unionAssetMarketMap = new Dictionary<Asset, List<string>>();
+            var unionNames = new List<string>();
+            
+            foreach (var asset in assets)
+            {
+                var assetMarkets = tickerResult.ToList().Where(i => i.Symbol.EndsWith(asset.Name)).Select(i => i.Symbol).OrderBy(i => i).ToList();
+                foreach (var name in assetMarkets)
+                {
+                    var unionName = name.Replace(asset.Name, "");
+                    unionNames.Add(unionName);
+                }                
+            }
+            Dictionary <string, int> text = new Dictionary<string, int>();
+            foreach (var str in unionNames)
+            {
+                if (text.ContainsKey(str))
+                    text[str]++;
+                else
+                    text.Add(str, 1);
+            }  
+            var fourValue = text.Where(i => i.Value == assets.Count()).ToList();
+
+            foreach (var asset in assets)
+            {
+                var unionN = new List<string>();
+                foreach (var four in fourValue)
+                {
+                    var assetMarkets = tickerResult.ToList().Where(i => i.Symbol.EndsWith(asset.Name) && i.Symbol.StartsWith(four.Key)).Select(i => i.Symbol).OrderBy(i => i).ToList();
+                    unionN.AddRange(assetMarkets);
+                }
+                unionAssetMarketMap.Add(asset, unionN);
+            }            
+            return unionAssetMarketMap;
         }
 
         /// <summary>
